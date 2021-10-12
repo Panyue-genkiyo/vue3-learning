25ec31673c2d109173da44f2127184b85c261ae2
panyue <90938238g@gmail.com>
2021-10-12 20:42:05 +0800

    初步分析vue3工程结构

---
+++ 01_src_分析工程结构/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--vue3的组件模版结构可以没有根标签-->
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

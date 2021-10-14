2a3620713be6e2baa147e4a7ff177b808a5bcb17
panyue <90938238g@gmail.com>
2021-10-14 12:50:04 +0800

    vue3判断响应式数据的api(isRef, isReactive, isReadonly, isProxy...)

---
+++ 19_src_响应式数据的判断/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app">
+  </div>
+</template>
+
+<script>
+import {reactive, readonly, ref, toRefs, isRef, isReactive, isReadonly, isProxy} from "vue";
+export default {
+  name: 'App',
+  setup(){
+
+    let car = reactive({
+      name: '宝马',
+      price: 40
+    });
+
+    let sum = ref(0);
+
+    let car2 = readonly(car); ///readonly依然返回代理类型的对象只不过它不能再改而已
+
+    console.log(isRef(sum), isReactive(car), isReadonly(car2), isProxy(car), isProxy(car2), isProxy(sum));
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.app{
+  background: grey;
+  padding: 10px;
+}
+</style>

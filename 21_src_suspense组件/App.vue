3dfe3e952812dd681b7bf6f4f3b871eeda46a393
panyue <90938238g@gmail.com>
2021-10-14 15:50:47 +0800

    总结vue3新组件(Suspense(异步引入组件))

---
+++ 21_src_suspense组件/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app">
+    <h2>app</h2>
+    <Suspense>
+      <template v-slot:default>
+        <Child/>
+      </template>
+      <template v-slot:fallback>
+        <!--退路-->
+        <h3>loading...</h3>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script>
+// import Child from "./components/Child"; 静态引入组件
+import { defineAsyncComponent } from "vue";
+const Child = defineAsyncComponent(() => import('./components/Child')); //动态引入组件(异步)
+export default {
+  name: 'App',
+  components: {Child},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.app{
+  background: grey;
+  padding: 10px;
+}
+</style>
---
+++ 21_src_suspense组件/components/Child.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="child">
+    <h2>child(子)</h2>
+    {{ sum }}
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: "Child",
+  async setup() {
+    let sum = ref(0);
+
+    let p = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          sum
+        });
+      },5000)
+    })
+
+    return await p;
+  }
+}
+</script>
+
+<style scoped>
+   .child{
+     background: skyblue;
+     padding: 10px;
+   }
+</style>
